c19e6417fc6b364c6f603d036093c770c36d7037
IMPATIENT89 <ankitshukla8948328509@gmail.com>
2023-07-20 01:56:17 +0530

    ti2

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module BoltRacers
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.assets.enabled = true
+    config.assets.precompile += %w( es-module-shims.min.js )
+
   end
 end
